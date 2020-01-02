d32805d1846e736829f46fddd5a82cd236d00ae9
Kota <44553544+kotamorishi@users.noreply.github.com>
2020-01-02 11:31:18 -0500

    Create 1_GameScene.swift

---
+++ 1_GameScene.swift
@@ -0,0 +1,32 @@
+//
+//  GameScene.swift
+//  tap tap game - sample 1
+//  
+//  YouTube : https://youtube.com/
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
